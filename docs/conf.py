1659b934b3e3c5593d38089e9567e62a7ddb5856
Eliseo Martelli <me@eliseomartelli.it>
2024-03-15 12:07:22 +0100

    fix: remove imports

--- docs/conf.py
+++ docs/conf.py
@@ -7,8 +7,8 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import os
-import sys
+# import os
+# import sys
 
 # -- Project information -----------------------------------------------------
 
